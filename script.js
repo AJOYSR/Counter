7d7452e4980d23e307b2ce7f303e6ccfc3e218e6
AJOYSR <jucse28.384@gmail.com>
2023-01-18 14:35:35 +0600

    calculator added

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-let count = 0;
-
-function setCount() {
-  document.getElementById("output").innerText = count;
+function setInitialValue() {
+  document.getElementById("input_marks").value = 0;
 }
 
-function setInitialCount() {
-  setCount();
-}
+function calculateGrade() {
+  const marks = parseInt(document.getElementById("input_marks").value);
 
-function increment() {
-  count++;
-  setCount();
-  if (count > 10) {
-    alert("Stoppppppppppp");
+  if (marks >= 80 && marks <= 100) {
+    document.getElementById("result").innerText = "Result is = A+";
+  }
+  if (marks >= 70 && marks <= 79) {
+    document.getElementById("result").innerText = "Result is = A";
+  }
+  if (marks >= 60 && marks <= 69) {
+    document.getElementById("result").innerText = "Result is = A-";
+  }
+  if (marks >= 50 && marks <= 59) {
+    document.getElementById("result").innerText = "Result is = B";
+  }
+  if (marks >= 40 && marks <= 49) {
+    document.getElementById("result").innerText = "Result is = C";
+  }
+  if (marks >= 0 && marks <= 39) {
+    document.getElementById("result").innerText = "Result is = F";
   }
 }
 
-setInitialCount();
+setInitialValue();
